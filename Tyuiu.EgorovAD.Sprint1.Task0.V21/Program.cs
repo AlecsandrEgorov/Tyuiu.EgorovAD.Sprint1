ef730c934403bf65bf8d62cb9198e78ca7537cfd
Tyuiu.EgorovAD@outlook.com <e.alexa07@mail.ru>
2025-10-01 21:11:13 +0300

    Add project files.

---
+++ Tyuiu.EgorovAD.Sprint1.Task0.V21/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.EgorovAD.Sprint1.Task0.V21.Lib;
+namespace Tyuiu.EgorovAD.Sprint1.Task0.v21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Егоров.А.Д. | СМАРТб25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навики работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил: Егоров.А.Д. | СМАРТб25-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 3*4+10/2                *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 3*4+10/2                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine();
+        }
+    }
+}
