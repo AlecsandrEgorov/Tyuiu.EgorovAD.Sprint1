9f3d3685c5af31b43242efaf2a66f27e12529904
Tyuiu.EgorovAD@outlook.com <e.alexa07@mail.ru>
2025-10-02 19:13:30 +0300

    Task2

---
+++ Tyuiu.EgorovAD.Sprint1.Task2.V10.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.EgorovAD.Sprint1.Task2.V10.Lib;
+namespace Tyuiu.EgorovAD.Sprint1.Task2.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 1;
+            var res = ds.ConvertMetreToInchs(x);
+            Assert.AreEqual(39.37, res);   
+
+        }
+    }
+}
---
+++ Tyuiu.EgorovAD.Sprint1.Task2.V10/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.EgorovAD.Sprint1.Task2.V10.Lib;
+namespace Tyuiu.EgorovAD.Sprint1.Task2.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Егоров.А.Д. | СМАРТб25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навики работы в C#                                        *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Егоров.А.Д. | СМАРТб25-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+            Console.WriteLine("Введите Количество метров:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.ConvertMetreToInchs(x));
+
+            Console.ReadLine();
+        }
+    }
+}
