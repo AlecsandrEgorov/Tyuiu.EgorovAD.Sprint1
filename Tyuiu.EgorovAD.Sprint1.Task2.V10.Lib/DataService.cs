82b0151cc00f56820207d42afa699e9db39811d5
Tyuiu.EgorovAD@outlook.com <e.alexa07@mail.ru>
2025-10-02 19:22:33 +0300

    Task2

--- Tyuiu.EgorovAD.Sprint1.Task2.V10.Lib/DataService.cs
+++ Tyuiu.EgorovAD.Sprint1.Task2.V10.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.EgorovAD.Sprint1.Task2.V10.Lib
         public double ConvertMetreToInchs(int value)
         {
             // перевод
-            return Math.Round(value * 39.37, 3);
+            return Math.Round(value * 39.37, 4);
         }
     }
 }
