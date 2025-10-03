6fb5489d116cc00c91657f00dc5fdc55cb8b1043
Tyuiu.EgorovAD@outlook.com <e.alexa07@mail.ru>
2025-10-03 18:15:39 +0300

    Task5

---
+++ Tyuiu.EgorovAD.Sprint1.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.EgorovAD.Sprint1.Task5.V1.Lib;
+namespace Tyuiu.EgorovAD.Sprint1.Task5.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x1 = 1;
+            double x2 = 4;
+            double y1 = 2;
+            double y2 = 6;
+            int wait = 5;
+            var res = ds.DistanceBetweenDots(x1, y1, x2, y2);
+            int result = Convert.ToInt32(res);
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.EgorovAD.Sprint1.Task5.V1/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.EgorovAD.Sprint1.Task5.V1.Lib;
+namespace Tyuiu.EgorovAD.Sprint1.Task5.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Егоров.А.Д. | СМАРТб25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навики работы в C#                                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Егоров.А.Д. | СМАРТб25-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double x1, y1, x2, y2;
+            Console.WriteLine("Введите x1:");
+            x1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите y1:");
+            y1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Введите x2:");
+            x2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите y2:");
+            y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = Convert.ToInt32(ds.DistanceBetweenDots(x1, y1, x2, y2));
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
