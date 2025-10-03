c198122f0cc359ce0576cd54e1a742786ab586d1
Tyuiu.EgorovAD@outlook.com <e.alexa07@mail.ru>
2025-10-03 19:30:30 +0300

    Task6

---
+++ Tyuiu.EgorovAD.Sprint1.Task6.V18.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.EgorovAD.Sprint1.Task6.V18.Lib;
+namespace Tyuiu.EgorovAD.Sprint1.Task6.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidString()
+        {
+            DataService ds = new DataService();
+            string str = "123";
+            bool res = ds.CheckNumber(str);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.EgorovAD.Sprint1.Task6.V18/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.EgorovAD.Sprint1.Task6.V18.Lib;
+namespace Tyuiu.EgorovAD.Sprint1.Task6.V18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Егоров.А.Д. | СМАРТб25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навики работы в C#                                        *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Егоров.А.Д. | СМАРТб25-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие:                                                                *");
+            Console.WriteLine("* Написать программу: пользователь вводит текст. Проверить,что строка     *");
+            Console.WriteLine("* символьным представлением натурального числа                            *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите строку:");
+            string str = Console.ReadLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.CheckNumber(value: str));
+
+            Console.ReadKey();
+        }
+    }
+}
